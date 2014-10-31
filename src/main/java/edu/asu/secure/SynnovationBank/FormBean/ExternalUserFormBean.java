c1ee43e8358ea0ade516f22b20d9a496d7b176cf
Rohit Kharat <rohit817@gmail.com>
2014-10-30 22:04:42 -0700

    added form bean for "Add External User"

---
+++ src/main/java/edu/asu/secure/SynnovationBank/FormBean/ExternalUserFormBean.java
@@ -0,0 +1,15 @@
+package edu.asu.secure.SynnovationBank.FormBean;
+
+public class ExternalUserFormBean {
+
+	private String fname;
+
+	public String getFname() {
+		return fname;
+	}
+
+	public void setFname(String fname) {
+		this.fname = fname;
+	}
+	
+}
