965fcbda7a8c09c911933091af3ba5f4243fbafd
MEGHNA <MEGHNA@MEGHNA-PC>
2014-11-01 15:47:49 -0700

    updated changes

--- src/main/java/edu/asu/secure/SynnovationBank/DaoImpl/AccountDAOImpl.java
+++ src/main/java/edu/asu/secure/SynnovationBank/DaoImpl/AccountDAOImpl.java
@@ -1,40 +1,30 @@
 package edu.asu.secure.SynnovationBank.DaoImpl;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import edu.asu.secure.SynnovationBank.DBUtilities.HibernateUtil;
 import edu.asu.secure.SynnovationBank.DTO.Account;
+import edu.asu.secure.SynnovationBank.DTO.Person;
 import edu.asu.secure.SynnovationBank.Dao.AccountDAO;
 
 @Repository
 public class AccountDAOImpl implements AccountDAO {
 
 	SessionFactory factory = HibernateUtil.buildSessionFactory();
-	
-	@Override
-	public long insertAccount(Account account) {
-		long accountNo = -1;
-		try{
-			Session session = factory.getCurrentSession();
-			session.beginTransaction();
-			accountNo = (Long)session.save(account);
-			session.getTransaction().commit();
-			return accountNo;
-		}
-		catch(Exception e){
-			e.printStackTrace();
-			return accountNo;
-		}
-	}
 
 	@Override
-	public boolean updateAccount(Account account) {
+	public boolean updateAccountBalance(Long accNo, float balance) {
 		try{
 			Session session = factory.getCurrentSession();
 			session.beginTransaction();
+			Account account = (Account)session.get(Account.class, accNo);
+			if(account != null)
+				account.setBalance(balance);
 			session.update(account);
 			session.getTransaction().commit();
 			return true;
@@ -43,8 +33,24 @@ public class AccountDAOImpl implements AccountDAO {
 			e.printStackTrace();
 			return false;
 		}
-
 	}
 
-	
+	@Override
+	public boolean fetchAllowAccessFlag(Long accNo) {
+		try{
+			Session session = factory.openSession();
+			Criteria criteria = session.createCriteria(Person.class);
+			criteria.add(Restrictions.eq("account.accountNumber",accNo));
+			Person person = (Person)criteria.uniqueResult();
+			if(person != null && person.getAllowAccessFlag().equals("Y"))
+				return true;
+			else
+				return false;
+		}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	}
+
 }
--- src/main/java/edu/asu/secure/SynnovationBank/ServiceImpl/OTPServiceImpl.java
+++ src/main/java/edu/asu/secure/SynnovationBank/ServiceImpl/OTPServiceImpl.java
@@ -5,7 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import edu.asu.secure.SynnovationBank.Dao.UserDaoTest;
+import edu.asu.secure.SynnovationBank.Dao.PersonDAO;
 import edu.asu.secure.SynnovationBank.Handler.OTPGeneratorHandler;
 import edu.asu.secure.SynnovationBank.Service.OTPService;
 import edu.asu.secure.SynnovationBank.hash.HashCode;
@@ -16,38 +16,34 @@ public class OTPServiceImpl implements OTPService{
 protected static Logger logger = Logger.getLogger("service");
 	
 	@Autowired
-	private UserDaoTest userDao;
+	private PersonDAO personDao;
 
 	@Override
 	public boolean validateUser(String username, String email) {
-		// TODO Auto-generated method stub
 		String otp=OTPGeneratorHandler.GenerateOTP();
-		if(userDao.updateOTP(username,email,otp)){
-			OTPGeneratorHandler.SendOTP(email, otp);
-			return true;
-		}
+//		if(personDao.updateOTP(username,email,otp)){
+//			OTPGeneratorHandler.SendOTP(email, otp);
+//			return true;
+//		}
 		return false;
 	}
 
 	@Override
 	public boolean compareOTP(String otp) {
 		
-		if(userDao.checkOTP(otp)){
-			return true;
-		}
+//		if(personDao.checkOTP(otp)){
+//			return true;
+//		}
 		return false;
-		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
 	public boolean updatePassword(String newpassword, String username) {
-		// TODO Auto-generated method stub
-		
 		String encodedPass=HashCode.getHashPassword(newpassword);
-		if(userDao.updatePassword(encodedPass, username)){
-			return true;
-		}
+//		if(personDao.updatePassword(encodedPass, username)){
+//			return true;
+//		}
 		return false;
 	}
 	
