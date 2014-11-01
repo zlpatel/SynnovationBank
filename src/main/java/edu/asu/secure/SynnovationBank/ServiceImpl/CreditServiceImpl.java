b5b25084b4019f0f105fae5da0439633c158a886
Vignesh Narayanan <vnaray15@asu.edu>
2014-11-01 14:04:12 -0700

    commiting changes in credit service impl

--- src/main/java/edu/asu/secure/SynnovationBank/ServiceImpl/CreditServiceImpl.java
+++ src/main/java/edu/asu/secure/SynnovationBank/ServiceImpl/CreditServiceImpl.java
@@ -3,13 +3,17 @@ package edu.asu.secure.SynnovationBank.ServiceImpl;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import edu.asu.secure.SynnovationBank.DTO.Account;
+import edu.asu.secure.SynnovationBank.DTO.Person;
 import edu.asu.secure.SynnovationBank.DTO.TransactionDetails;
 import edu.asu.secure.SynnovationBank.DTO.TransactionType;
 import edu.asu.secure.SynnovationBank.DTO.Transactions;
+import edu.asu.secure.SynnovationBank.Dao.AccountDAO;
+import edu.asu.secure.SynnovationBank.Dao.PersonDAO;
 import edu.asu.secure.SynnovationBank.Service.CreditService;
 
 @Service
@@ -18,16 +22,33 @@ public class CreditServiceImpl implements CreditService {
 
 	
 
+	@Autowired
+	private PersonDAO personDAO;
+	@Autowired
+	private AccountDAO accountDAO;
 	
 
 	@Override
 	public boolean creditAmount(String amount) {
+		
+		
+		Person sender = personDAO.fetchPersonById("customer");
+		Account a=(Account) sender.getAccount();
+		float balance=a.getBalance();
+		float credit=Float.parseFloat(amount);
+		float new_balance=balance+credit;
+		a.setBalance(new_balance);
+		accountDAO.updateAccount(a);
+		System.out.println("Updated customer account with new credit!");
+		
+		
 		/*
 		
 		// TODO Auto-generated method stub
 		
 		
 		//ACCOUNT BALANCE MODIFICATION
+		
 		
 		
 		Account a = null;
