784840dd17c094a2d65de37df45ebcf9b1f834e3
pratik <pratik@pratiks-mbp-2.mobile.asu.edu>
2014-11-02 14:20:53 -0700

    TechAccountAccessServiceImpl

--- src/main/java/edu/asu/secure/SynnovationBank/ServiceImpl/TechAccountAccessServiceImpl.java
+++ src/main/java/edu/asu/secure/SynnovationBank/ServiceImpl/TechAccountAccessServiceImpl.java
@@ -4,7 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.sun.nio.sctp.Notification;
+//import com.sun.nio.sctp.Notification;
 
 import edu.asu.secure.SynnovationBank.DTO.Notifications;
 import edu.asu.secure.SynnovationBank.DTO.NotificationsType;
