898f704b088f3c7713f10055516f7f95d7222907
zeel patel <zvpatel@asu.edu>
2014-11-02 03:36:14 -0700

    File Uploading mechanism

--- src/main/java/edu/asu/secure/SynnovationBank/Controller/FileUploadController.java
+++ src/main/java/edu/asu/secure/SynnovationBank/Controller/FileUploadController.java
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
-import edu.asu.secure.SynnovationBank.FormBean.FileUploadFormBean;
 
 @Controller
 @RequestMapping("/pki")
@@ -22,7 +22,7 @@ public class FileUploadController {
 
 	protected static Logger logger = Logger.getLogger("controller");
 	
-	@RequestMapping(value = "/fileUploader", method = RequestMethod.GET)
+	@RequestMapping(value = "/fileUploader")
 	public String getUploadFilePage(@RequestParam(value="error", required=false) boolean error,ModelMap model ) {
 		
 		if(error==true){
@@ -33,42 +33,39 @@ public class FileUploadController {
 		logger.debug("Received request to show fileUpload page");
     	return "fileUpload";
 	}
-	@RequestMapping(value = "/uploadFile", method = RequestMethod.POST)
-	public String uploadFile(@ModelAttribute("fileuploadformbean")FileUploadFormBean fileUploadFormBean,BindingResult result,ModelMap model ) {
-		MultipartFile file=fileUploadFormBean.getFile();
-		String name=fileUploadFormBean.getName();
-		
+	@RequestMapping(value = "/uploadFile", method = RequestMethod.GET)
+	public @ResponseBody
+	String uploadFileHandler(@RequestParam("name") String name,
+			@RequestParam("file") MultipartFile file) {
+
 		if (!file.isEmpty()) {
-            try {
-                byte[] bytes = file.getBytes();
- 
-                // Creating the directory to store file
-                String rootPath = System.getProperty("catalina.home");
-                File dir = new File(rootPath + File.separator + "tmpFiles");
-                if (!dir.exists())
-                    dir.mkdirs();
- 
-                // Create the file on server
-                File serverFile = new File(dir.getAbsolutePath()
-                        + File.separator + name);
-                BufferedOutputStream stream = new BufferedOutputStream(
-                        new FileOutputStream(serverFile));
-                stream.write(bytes);
-                stream.close();
- 
-                logger.info("Server File Location="
-                        + serverFile.getAbsolutePath());
- 
-                return "redirect:auth/login";
-            } catch (Exception e) {
-            	model.put("error", true);
-                return "redirect:fileUploader";
-            }
-        } else {
-        	model.put("error", true);
-            return "redirect:fileUploader";
-        }
-		
-    	
+			try {
+				byte[] bytes = file.getBytes();
+
+				// Creating the directory to store file
+				String rootPath = System.getProperty("catalina.home");
+				File dir = new File(rootPath + File.separator + "tmpFiles");
+				if (!dir.exists())
+					dir.mkdirs();
+
+				// Create the file on server
+				File serverFile = new File(dir.getAbsolutePath()
+						+ File.separator + name);
+				BufferedOutputStream stream = new BufferedOutputStream(
+						new FileOutputStream(serverFile));
+				stream.write(bytes);
+				stream.close();
+
+				logger.info("Server File Location="
+						+ serverFile.getAbsolutePath());
+
+				return "You successfully uploaded file=" + name;
+			} catch (Exception e) {
+				return "You failed to upload " + name + " => " + e.getMessage();
+			}
+		} else {
+			return "You failed to upload " + name
+					+ " because the file was empty.";
+		}
 	}
 }
